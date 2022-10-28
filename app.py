61735f115e814e8ffe6ad4addf50ec0e3232b8f5
Karthikeyan K <110445793+karthikeyankdeveloper@users.noreply.github.com>
2022-10-28 21:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 # conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=764264db-9824-4b7c-82df-40d1b13897c2.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32536;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=hhn68702;PWD=JDmq4keRKWs2f0Mi", '', '')
 # end of ibm
 
+
 app = Flask(__name__) 
 
 @app.route('/')
